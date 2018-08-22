b48abd6cc57c6554b543d621f86cbf7dec59fe8a
Patryk Mroczyński <mroczynskip@gmail.com>
2018-08-22 19:09:53 +0200

    Update GUI

--- src/gui.py
+++ src/gui.py
@@ -3,6 +3,8 @@ from tkinter import messagebox
 from tkinter.filedialog import askopenfilename
 from pytler import Pytler
 
+#TODO tworzenie i odświeżanie sesji
+
 class GUI:
     def __init__(self):
         self.font = ('helvetica', 13, 'bold')
@@ -35,23 +37,23 @@ class GUI:
         login_button.pack(fill=BOTH, expand=1, side=LEFT)
         register_button.pack(fill=BOTH, expand=1, side=LEFT)
 
-    def create_button(self, text, master, action= None, height=80, width=120, side=LEFT, pady=(0,0), padx=(0,0)):
+    def create_button(self, text, master, action= None, height=80, width=120, side=LEFT, pady=(0,0), padx=(0,0), bg='white', fg='black'):
         f = Frame(master=master, height=height, width=width)
         f.pack_propagate(0)
         f.pack(side=side, padx=padx, pady=pady)
-        return Button(master=f, text=text, command=action, bg='white', fg='black', font=self.font)
+        return Button(master=f, text=text, command=action, bg=bg, fg=fg, font=self.font)
 
-    def create_entry(self, master, height=40, width=120, side=LEFT, pady=(0,0), padx=(0,0)):
+    def create_entry(self, master, height=40, width=120, side=LEFT, pady=(0,0), padx=(0,0), bg='white', fg='black'):
         f = Frame(master=master, height=height, width=width)
         f.pack_propagate(0)
         f.pack(side=side, padx=padx, pady=pady)
-        return Entry(master=f, bg='white', fg='black')
+        return Entry(master=f, bg=bg, fg=fg)
 
-    def create_label(self, text, master, height=40, width=130, side=LEFT, pady=(0,0), padx=(0,0)):
+    def create_label(self, text, master, height=40, width=130, side=LEFT, pady=(0,0), padx=(0,0), bg='white', fg='black'):
         f = Frame(master=master, height=height, width=width)
         f.pack_propagate(0)
         f.pack(side=side, padx=padx, pady=pady)
-        return Label(master=f, text=text, bg='white', fg='black')
+        return Label(master=f, text=text, bg=bg, fg=fg, font=self.font)
 
     def view_register(self):
         self.reset_back()
@@ -183,7 +185,81 @@ class GUI:
 
     def main_view(self):
         self.reset_back()
-        self.root.geometry('340x160')
+        self.root.geometry('1000x500')
+
+
+        contact_frame = Frame(master=self.back, bg='black', height=500, width=200)
+        contact_frame.pack_propagate(0)
+        contact_frame.pack(side=LEFT)
+
+        main_frame = Frame(master=self.back, bg='green', height=500, width=800)
+        main_frame.pack_propagate(0)
+        main_frame.pack(side=LEFT)
+
+        self.generate_contacts(contact_frame)
+
+        menu_frame = Frame(master=main_frame, bg='black', width=800, height=70)
+        menu_frame.pack_propagate(0)
+        menu_frame.pack()
+
+        self.generate_topbar_content(menu_frame)
+
+        content_frame = Frame(master=main_frame, bg='blue', width=800, height=350)
+        content_frame.pack_propagate(0)
+        content_frame.pack()
+
+        action_bar = Frame(master=main_frame, bg='green', width=800, height=80)
+        action_bar.pack_propagate(0)
+        action_bar.pack()
+
+    def generate_contacts(self, root):
+        r = self.pytler.get_contacts()
+        if not r:
+            messagebox.showwarning('Błąd', 'Błąd podczas wczytywania kontaktów')
+
+        frame = Frame(master=root, bg='black')
+        frame.pack()
+        label = self.create_label('Kontakty', frame, width = 200, pady=(20,0), fg='white', bg='black')
+        label.pack(fill=BOTH, expand=1, side=TOP)
+
+        frame2 = Frame(master=root, bg='black', height=350, width=200)
+        frame2.pack(pady=10)
+        frame2.pack_propagate(0)
+        scrollbar = Scrollbar(frame2)
+        scrollbar.pack(fill=BOTH, expand=1, side=RIGHT)
+        contact_list = Listbox(frame2, yscrollcommand = scrollbar.set, width= 180, bg='black', font=self.font, fg='white')
+        contact_list.insert(END, 'login')
+        if self.pytler.contacts:
+            for contact in self.pytler.contacts:
+                contact_list.insert(END, contact['login'])
+        contact_list.pack( side = LEFT, fill = BOTH )
+
+        contact_list.bind('<<ListboxSelect>>', lambda evt: self.onselect(evt, 1))
+
+        frame1 = Frame(master=root, bg='black')
+        frame1.pack()
+        add_button = self.create_button('Dodaj kontakt +', frame1, width = 150, height=60, fg='black', bg='white')
+        add_button.pack(fill=BOTH, expand=1, side=TOP)
+
+    def generate_topbar_content(self, topbar):
+
+        home = self.create_button('Strona główna', topbar, padx=(300,20), height=50)
+        home.pack(fill=BOTH, expand=1, side=RIGHT)
+
+        settings = self.create_button('Ustawienia', topbar, padx=(20,20), height=50)
+        settings.pack(fill=BOTH, expand=1, side=RIGHT)
+
+        logout = self.create_button('Wyloguj', topbar, padx=(20,20), height=50)
+        logout.pack(fill=BOTH, expand=1, side=RIGHT)
+
+    def onselect(self, evt, a):
+        #TODO wyświetlanie info
+        w = evt.widget
+        index = int(w.curselection()[0])
+        value = w.get(index)
+        print('You selected item %d: "%s"' % (index, value))
+
+
 
 
     def main(self):
